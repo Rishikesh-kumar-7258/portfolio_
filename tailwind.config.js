4fb532919ab5ba910bcfb57a4052f80afe5cda37
rishi <rishi7258prince@gmail.com>
2026-06-18 20:13:25 +0530

    Feat: Updated MTP details

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html', './js/**/*.js'],
+  content: ['./*.html', './pages/**/*.html', './js/**/*.js'],
   theme: {
     extend: {
       colors: {
